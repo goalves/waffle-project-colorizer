150e5791ebd28baea6561119397a8b7d5efa677e
Gabriel Alves <g04lves@gmail.com>
2018-09-24 18:10:40 -0300

    Fix README, setup Google Store, remakes README.md.

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
 Chrome Extension to add colors to different projects that are being represented in a single waffle.io board.
 
 ## How to install:
+
+### Installing from releases:
 To install you need to go through these steps:
 1. Download the latest version from the [releases](https://github.com/goalves/waffle-project-colorizer/releases "WPC-Releases") section of this repository.
 2. Open chrome extensions settings (chrome://extensions/) and just drag and drop the `*.cxr` file that was downloaded.
 
+### Installing from Google Store:
+Just install from [here](https://chrome.google.com/webstore/detail/waffle-colorizer/ahnlhnialcfkgdpaiilnjblenjbhgdmo?authuser=1).
+
 ## How to use:
 Currently this version automatically starts working once you open a board in Waffle.
 It is still possible to change the color of all cards clicking in the painting palette added to your other chrome extensions.
+
+## How to test and make changes:
+Clone this repo and access chrome://extensions.
+Click on developer mode and then `Load unpacked`, select the folder and reload the code when you want.
--- content-scripts.js
+++ content-scripts.js
@@ -1,72 +1,75 @@
-function colorize() {
-  cardProjectDiv = $(this).find('div.source-name.ng-scope')
-  cardNumberDiv = $(this).find('button.card-number.btn.btn-link.btn-xs')
-  cardProject = cardProjectDiv.text()
+var availableColors = []
+var projectsList = []
+var projectCount = 0
 
-  if(typeof localStorage[cardProject] == "undefined") {
+function colorize () {
+  var cardProjectDiv = $(this).find('div.source-name.ng-scope')
+  var cardNumberDiv = $(this).find('button.card-number.btn.btn-link.btn-xs')
+  var cardProject = cardProjectDiv.text()
+
+  if (typeof localStorage[cardProject] === 'undefined') {
     localStorage[cardProject] = getColor(cardProject)
   }
-  color = localStorage[cardProject]
-  $(this).css('background-color',color)
-  cardProjectDiv.css('color',"#333333")
-  cardNumberDiv.css('color',"#333333")
+
+  var color = localStorage[cardProject]
+  $(this).css('background-color', color)
+  cardProjectDiv.css('color', '#333333')
+  cardNumberDiv.css('color', '#333333')
 }
 
-function getColor(cardProject) {
+function getColor (cardProject) {
   var color = availableColors[projectCount]
   projectCount++
-  return "hsl(" + color + ",100%,85%)";
+  return 'hsl(' + color + ',100%,85%)'
 }
 
-function calculateColorHues(){
-  if(projectCount > 1){
-    startPoint = (360/projectCount)/2
-    number = 360/(projectCount)
-    for(i = startPoint; i <= 360; i = i+number){
+function calculateColorHues () {
+  if (projectCount > 1) {
+    var startPoint = (360 / projectCount) / 2
+    var number = 360 / (projectCount)
+    var i
+    for (i = startPoint; i <= 360; i = i + number) {
       availableColors.push(Math.floor(i))
     }
   }
 }
 
-function checkNewProject() {
-  cardProjectDiv = $(this).find('div.source-name.ng-scope')
-  cardProject = cardProjectDiv.text()
+function checkNewProject () {
+  var cardProjectDiv = $(this).find('div.source-name.ng-scope')
+  var cardProject = cardProjectDiv.text()
 
-  if(!isInArray(cardProject, projectsList)) {
+  if (!isInArray(cardProject, projectsList)) {
     projectsList.push(cardProject)
     projectCount++
   }
 }
 
-function isInArray(value, array) {
-  return array.indexOf(value) > -1;
+function isInArray (value, array) {
+  return array.indexOf(value) > -1
 }
 
 var observer = new MutationObserver(function (MutationRecords, MutationObserver) {
-  start();
-});
+  start()
+})
 
 observer.observe($('body').get(0), {
-  childList: true,
-});
+  childList: true
+})
 
-function start(){
-  //Clear previous data
+function cleanup () {
   localStorage.clear()
   projectsList = []
   availableColors = []
   projectCount = 0
+}
 
-  //Get info from card-headers
+function start () {
+  cleanup()
+
   $('.card-header').each(checkNewProject)
   calculateColorHues(projectCount)
   console.log(availableColors)
   projectCount = 0
 
-  //Colorize
   $('.card-header').each(colorize)
 }
-
-var availableColors = []
-var projectsList = []
-var projectCount = 0;
